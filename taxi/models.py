9abc1d7255210a85885d950041273d3d465a8516
Tymur <shmorguntimur@gmail.com>
2025-03-01 01:59:12 +0200

    solution

--- taxi/models.py
+++ taxi/models.py
@@ -10,7 +10,7 @@ class Manufacturer(models.Model):
         return self.name
 
 
-class Driver(AbstractUser):  # Inherits from AbstractUser
+class Driver(AbstractUser):
     license_number = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
